b3a93f2ca2dcef30e51b3282c2b0d8c973093d47
Guilherme Gutierres Gallo <guilherme.gg1@hotmail.com>
2022-01-03 09:08:31 -0300

    fix(cambio): menu bg and glitch (#64)

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,16 @@
-const clamp = (value, min, max) => Math.max(Math.min(value, max), min)
+export const clamp = (value, min, max) => Math.max(Math.min(value, max), min)
 
-export { clamp }
+export function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+export function randomRGBA(opacity = 0.2) {
+  return `
+    rgba(
+      ${randomIntFromInterval(0, 255)},
+      ${randomIntFromInterval(0, 255)},
+      ${randomIntFromInterval(0, 255)},
+      ${opacity}
+    )
+  `
+}
