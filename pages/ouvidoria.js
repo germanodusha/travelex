c89b658fd56467b193c5e4b90b25efbb8fc5848b
Guilherme Gutierres Gallo <guilherme.gg1@hotmail.com>
2022-01-04 17:28:16 -0300

    feat(md): add markdown to pages (#63)
    * feat(md): add markdown to ouvidoria's page
    
    * feat(md): add markdown to compliance's page
    
    * fix: merge error
    
    * feat(md): add markdown to cambio pages

--- pages/ouvidoria.js
+++ pages/ouvidoria.js
@@ -1,25 +1,29 @@
 import { useTranslations } from 'use-intl'
+import { useRouter } from 'next/router'
+import Interweave from 'interweave'
 import UsefulPageLegacy from '@/components/UsefulPageLegacy'
+import { readMarkdownLocale } from '@/utils/markdown'
 
-function Ouvidoria() {
+function Ouvidoria({ markdown }) {
+  const { locale } = useRouter()
   const translate = useTranslations('Ouvidoria')
 
   return (
-    <UsefulPageLegacy
-      title={translate('title')}
-      caption={translate('caption')}
-      content={translate('content')}
-    />
+    <UsefulPageLegacy title={translate('title')} caption={translate('caption')}>
+      <Interweave noWrap content={markdown[locale]} />
+    </UsefulPageLegacy>
   )
 }
 
 export function getStaticProps({ locale }) {
   const messages = require(`../content/${locale}.json`)
+  const markdown = readMarkdownLocale('ouvidoria')
 
   return {
     props: {
       messages,
       locale,
+      markdown,
     },
   }
 }
